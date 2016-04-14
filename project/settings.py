3b7f8d084af1b1ef00be644188b9b6468688480b
Nicholas Maselli <Nicholas Maselli>
2016-04-14 15:10:58 -0400

    changing credits to remote

--- project/settings.py
+++ project/settings.py
@@ -2,7 +2,10 @@
 Django Settings File
 '''
 import os
-import localcreds
+import sys
+#sys.path.append("/cygdrive/c/Users/masel_000/Desktop/COS333/Application/Application/project")
+
+#import localcreds
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -21,7 +24,7 @@ def SECRET_KEYS(deploy):
 #Toggle 'LOCAL' if you're developing locally via $ heroku local
 #Be sure to run localcreds.py in the project directory before
 #developing locally! (instructions in localcreds.py source code) 
-(SECRET_KEY, FIREBASE_KEY) = SECRET_KEYS('LOCAL')
+(SECRET_KEY, FIREBASE_KEY) = SECRET_KEYS('REMOTE')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
